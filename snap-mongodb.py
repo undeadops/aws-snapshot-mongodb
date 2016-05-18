d634f5d38dec8c1c38040b8f22ac886190da9bb8
Mitch Anderson <mitch@nuvi.com>
2016-05-18 10:59:08 -0600

    fixed all my broken

--- snap-mongodb.py
+++ snap-mongodb.py
@@ -10,12 +10,15 @@ from datetime import datetime
 import time
 import sys
 import logging
+
+from pymongo import MongoClient
+import boto3
 #from config import config
 
 def check_if_primary(config):
-    mongo_uri = "mongodb://%(username)s:%(password)s@localhost/admin" % { username: config.user, password: config.pass }
+    mongo_uri = "mongodb://%(username)s:%(password)s@localhost/admin" % { username: config.user, password: config.password }
     logging.debug("Connecting to MonogoDB: %s" % mongo_uri)
-    client = MongoClient(nongo_uri)
+    client = MongoClient(mongo_uri)
     try:
         return client.is_primary
     except:
@@ -40,23 +43,23 @@ def main():
     Create Snapshots of attached mongodb volumes - based on EC2 EBS Tag
     """
     parser = argparse.ArgumentParser(description='Snapshot MongoDB EBS RAID disks')
-    parser.add_argument('--user', action="store", help="Local MongoDB user")
-    parser.add_argument('--pass', action="store", help="Local MongoDB password")
-    parser.add_argument('--primary', action='store_true', default=false, help="Take Snapshot even in MongoDB is Primary")
-    parser.add_argument('--loglevel', action="store", default='WARNING', help="Log Level")
+    parser.add_argument('-u', dest="user", help="Local MongoDB user")
+    parser.add_argument('-p', dest="password", help="Local MongoDB password")
+    parser.add_argument('--primary', action='store_true', dest="is_primary", default=False, help="Take Snapshot even in MongoDB is Primary")
+    parser.add_argument('--loglevel', action="store", dest="loglevel", default='INFO', help="Log Level")
     config = parser.parse_args()
-    loglevel = "logging.%s" % config.loglevel
+    loglevel = getattr(logging, config.loglevel.upper())
     logging.basicConfig(filename='/var/log/snapshot-mongodb.log', level=loglevel )
     # TODO: Better way to do this... likely...
-    if check_if_primary(config) and not config.primary:
+    if check_if_primary(config) and not config.is_primary:
         logging.info("%s - Local MongoDB is current Primary - Not running snapshot" % curdate())
         sys.exit(0)
     logging.info("%s - Starting MongoDB disk snapshots" % curdate())
     # Get my instance ID
     instance = get_instance()
     logging.debug("%(date)s - Instance %(instance_id)s" % {
-                        date: curdate(),
-                        instance_id: instance['Reservations'][0]['Instances'][0]['InstanceId']
+                        "date": curdate(),
+                        "instance_id": instance['Reservations'][0]['Instances'][0]['InstanceId']
                         } )
 
 
